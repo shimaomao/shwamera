561109981c5e33158b2a799a68c9c1b14c387775
Nazin <sernazin@merann.ru>
2017-10-26 12:10:57 +0300

    99%

--- src/main/java/ru/mera/sergeynazin/App.java
+++ src/main/java/ru/mera/sergeynazin/App.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 
 public class App {
     public static void main(String[] args) {
+        // FIXME: Nope!
         ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+
 
     }
 }
